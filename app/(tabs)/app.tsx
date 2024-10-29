e2ce0edb8980b7b05ecea293f1ac531c0eda9405
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2024-10-29 22:16:29 +0600

    environment setup for react native

---
+++ app/(tabs)/app.tsx
@@ -0,0 +1,20 @@
+// import PushNotification from 'react-native-push-notification';
+// import { Platform } from 'react-native';
+
+// // Configuration for Push Notifications
+// PushNotification.configure({
+//     onNotification: function (notification) {
+//         console.log("Notification received:", notification);
+//     },
+
+//     popInitialNotification: true,  // Set to true to handle initial notifications
+//     requestPermissions: Platform.OS === 'ios',
+// });
+
+// PushNotification.popInitialNotification((notification) => {
+//     if (notification) {
+//         console.log('Initial Notification', notification);
+//     } else {
+//         console.log('No initial notification received.');
+//     }
+// });
